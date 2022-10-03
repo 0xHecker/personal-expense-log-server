82886760162776150e5568b3a3a691f07896ab34
Sai Shanmukh <msshanmukh@gmail.com>
2022-10-03 19:08:04 +0530

    docker

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,15 @@ app.use(
     credentials: true,
   }),
 );
+app.all('*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,Content-Length, Authorization, Accept,X-Requested-With',
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  //...
+});
 
 app.set('trust proxy', 1);
 console.log(process.env.CORS_ORIGIN);
